82033b25d069e212863233b1f79b99d070681282
Durga-prasad-1 <147145773+Durga-prasad-1@users.noreply.github.com>
2025-05-08 15:02:36 +0530

    Update settings.py

--- webScrapy/table_extractor/table_extractor/settings.py
+++ webScrapy/table_extractor/table_extractor/settings.py
@@ -50,9 +50,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "table_extractor.middlewares.TableExtractorDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "table_extractor.middlewares.TableExtractorDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
